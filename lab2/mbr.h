9a00037e49c74937b2aaddff3f2c7cb9f4f9492f
timlathy <tim.labushev@gmail.com>
2021-03-10 20:07:38 +0300

    lab2: update partitions

--- lab2/mbr.h
+++ lab2/mbr.h
@@ -1,8 +1,10 @@
 #define DISK_SIZE_BYTES (55 * 1024 * 1024) // 50MiB
 
+#define MIB_TO_SECTORS(mib) (mib * 1024 * 1024 / 512)
+
 #define PARTITION_TABLE_OFFSET 446
-#define PARTITION_ENTRY_SIZE 16 
-#define PARTITION_TABLE_SIZE 64 
+#define PARTITION_ENTRY_SIZE 16
+#define PARTITION_TABLE_SIZE 64
 
 #define MBR_SIGNATURE_OFFSET 510
 #define MBR_SIGNATURE_SIZE 2
@@ -12,13 +14,13 @@ typedef struct
 {
 	unsigned char boot_type; // 0x00 - Inactive; 0x80 - Active (Bootable)
 	unsigned char start_head;
-	unsigned char start_sec:6;
-	unsigned char start_cyl_hi:2;
+	unsigned char start_sec : 6;
+	unsigned char start_cyl_hi : 2;
 	unsigned char start_cyl;
 	unsigned char part_type;
 	unsigned char end_head;
-	unsigned char end_sec:6;
-	unsigned char end_cyl_hi:2;
+	unsigned char end_sec : 6;
+	unsigned char end_cyl_hi : 2;
 	unsigned char end_cyl;
 	unsigned int abs_start_sec;
 	unsigned int sec_in_part;
@@ -26,50 +28,48 @@ typedef struct
 
 typedef PartEntry PartTable[4];
 
-static PartTable part_table =
-{
+static PartTable part_table = {
 	{
-		boot_type: 0x0,
-		start_sec: 0x0,
-		start_head: 0x0,
-		start_cyl: 0x0,
-		part_type: 0x83,
-		end_head: 0x0,
-		end_sec: 0x0,
-		end_cyl: 0x0,
-		abs_start_sec: 0x1,
-		sec_in_part: 0x4C4C // 10Mb
+		boot_type : 0x0,
+		start_sec : 0x0,
+		start_head : 0x0,
+		start_cyl : 0x0,
+		part_type : 0x83,
+		end_head : 0x0,
+		end_sec : 0x0,
+		end_cyl : 0x0,
+		abs_start_sec : 0x1,
+		sec_in_part : MIB_TO_SECTORS(10)
 	},
 	{
-		boot_type: 0x0,
-		start_head: 0x0,
-		start_sec: 0x0,
-		start_cyl: 0x0,
-		part_type: 0x83,
-		end_sec: 0x0,
-		end_head: 0x0,
-		end_cyl: 0x0,
-		abs_start_sec: 0x4C4D,
-		sec_in_part: 0x74C22 // 25Mb
+		boot_type : 0x0,
+		start_head : 0x0,
+		start_sec : 0x0,
+		start_cyl : 0x0,
+		part_type : 0x83,
+		end_sec : 0x0,
+		end_head : 0x0,
+		end_cyl : 0x0,
+		abs_start_sec : MIB_TO_SECTORS(10) + 1,
+		sec_in_part : MIB_TO_SECTORS(25)
 	},
-        {
-                boot_type: 0x0,
-                start_head: 0x0,
-                start_sec: 0x0,
-                start_cyl: 0x0,
-                part_type: 0x83,
-                end_sec: 0x0,
-                end_head: 0x0,
-                end_cyl: 0x0,
-                abs_start_sec: 0x7986F,
-                sec_in_part: 0x4244B // 15 Mb
-        }
-
+	{
+		boot_type : 0x0,
+		start_head : 0x0,
+		start_sec : 0x0,
+		start_cyl : 0x0,
+		part_type : 0x83,
+		end_sec : 0x0,
+		end_head : 0x0,
+		end_cyl : 0x0,
+		abs_start_sec : MIB_TO_SECTORS(35) + 1,
+		sec_in_part : MIB_TO_SECTORS(15)
+	}
 };
 
 inline void fill_partition_table(void* disk)
 {
 	memset(disk, 0x0, DISK_SIZE_BYTES);
 	memcpy(disk + PARTITION_TABLE_OFFSET, &part_table, PARTITION_TABLE_SIZE);
-	*(unsigned short *)(disk + MBR_SIGNATURE_OFFSET) = MBR_SIGNATURE;
+	*(unsigned short*)(disk + MBR_SIGNATURE_OFFSET) = MBR_SIGNATURE;
 }
